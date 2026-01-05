dbefc2a1989a2e35eead8f83167fa8760c72455c
Malki <malki@fexcon.com>
2026-01-05 23:09:28 +0530

    Make the portfolio public

--- portfolio/src/components/MyServices.js
+++ portfolio/src/components/MyServices.js
@@ -18,21 +18,22 @@ function MyServices() {
             <h2>My Services</h2>
            
            <div className="services-list">
+              <div>
+                <BsApple size={50} fontWeight={300} color="rgba(227, 19, 242, 0.742)"/>
+                <h3>Mobile App Design</h3>
+                <p>Develop and deliver robust cross-platform mobile apps, covering the entire development lifecycle—from design and coding to testing, deployment, and publishing on both Google Play Store and Apple App Store.</p>
+             </div>
              <div>
                 <BsCode size={50} fontWeight={500} color="rgba(227, 19, 242, 0.742)"  />
-                <h3>Frontend Development</h3>
-                <p>Craft responsive and dynamic web interfaces that not only captivate users visually but also ensure smooth, intuitive, and efficient interactions across all devices.</p>
+                <h3>Full Stack Development</h3>
+                <p>Design and implement full-stack web applications, creating responsive and dynamic interfaces while ensuring seamless backend integration, efficient performance, and intuitive user experiences across all devices.</p>
              </div>
              <div>
                <BsCrop size={50} fontWeight={500} color="rgba(227, 19, 242, 0.742)"  />
-                <h3>Web Design</h3>
-                <p>Utilizing the latest design trends & technologies,ensure the site is responsive, fast-loading, and optimized for an exceptional user experience across all devices</p>
+                <h3>AI & Chatbot Development</h3>
+                <p>Leverage the latest AI and NLP technologies to design and develop intelligent chatbots and virtual assistants, delivering seamless, interactive, and personalized user experiences across web and mobile platforms.</p>
              </div>
-             <div>
-                <BsApple size={50} fontWeight={300} color="rgba(227, 19, 242, 0.742)"/>
-                <h3>Mobile App Design</h3>
-                <p>Prioritize intuitive navigation, aesthetic appeal, and seamless performance to ensure the app stands out in a crowded market.</p>
-             </div>
+           
            </div>
           </div>
         </Col>
